1250bd1365e8ab868396f133679429cd80183ea3
klema <klimkonnov2004@mail.ru>
2025-04-06 20:41:42 +0300

    feat(app): dnd & Redux
    Redux implementation and drag and drop for burger ingredients

---
+++ src/index.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './component/app/app';
+import './styles.css';
+
+import {
+	compose,
+	legacy_createStore as createStore,
+	applyMiddleware,
+} from 'redux';
+import { Provider } from 'react-redux';
+import { rootReducer } from './services/reducers/reducers';
+import { thunk } from 'redux-thunk';
+
+const composeEnhancers =
+	typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+		: compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, enhancer);
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(
+	<StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</StrictMode>
+);
